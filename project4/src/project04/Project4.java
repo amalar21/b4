d9eec4c6f13a5f9e73f4afe6de4c2d7a2893a60a
Amala <amalar0521@gmail.com>
2024-06-26 11:14:38 +0530

    first commit

---
+++ project4/src/project04/Project4.java
@@ -0,0 +1,20 @@
+package project04;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Project4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("launch facebook");
+		System.setProperty("webdriver.gecko.driver","C:\\Users\\USER\\Downloads\\geckodriver-v0.26.0-win64\\geckodriver.exe");
+		WebDriver driver=new FirefoxDriver();
+		
+		
+		
+		
+		
+	}
+
+}
